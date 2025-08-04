ff6678430cd1b30a94f9c920efbee26332ceaf05
Aiden Storey <git@storey.dev>
2025-08-03 23:20:19 -0400

    Add buttons

--- scenes/level_data.gd
+++ scenes/level_data.gd
@@ -19,5 +19,25 @@ func _init(
 	for cel : String in parts[0].split(","):
 		data.append(int(cel))
 
+	for trigger_data : String in parts[1].split("/"):
+		var trigger_parts : PackedStringArray = trigger_data.split(":")
+
+		var trigger_coord : Vector2i = __parse_coord(trigger_parts[0])
+		buttons[trigger_coord] = []
+
+		for target_data : String in trigger_parts[1].split("|"):
+			buttons[trigger_coord].append(__parse_coord(target_data))
+
 	best_instruction = int(parts[2])
 	best_move = int(parts[3])
+
+
+func __parse_coord(
+	p_coord_string : String,
+) -> Vector2i:
+	var parts : PackedStringArray = p_coord_string.split(",")
+
+	return Vector2i(
+		int(parts[0]),
+		int(parts[1]),
+	)
